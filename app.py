cf78f519cc78a37501e2e831cb4268a7b6312ce4
arghasen10 <arghasen10@gmail.com>
2018-08-26 06:35:35 +0530

    best match with user confirmation

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import json
+from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(w):
     if w in data:
         return data[w]
+    elif len(get_close_matches(w,data.keys()))>0:
+        response =  input("Did you mean %s instead.Enter Y if yes and N if No." %get_close_matches(w,data.keys())[0])
+        if response == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        else:
+            return "the word does not exist.Please Double check again."
+
     else:
-        print("word does not exist.Please double check it")
+        return "word does not exist.Please double check it"
 
 word = input("Enter a word:")
 print(translate(word.lower()))
