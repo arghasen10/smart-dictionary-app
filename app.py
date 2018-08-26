b46287bc797a18392e40921379ad8014cef36988
arghasen10 <arghasen10@gmail.com>
2018-08-26 06:48:34 +0530

    print more optimised way

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(w):
+    w = w.lower()
     if w in data:
         return data[w]
     elif len(get_close_matches(w,data.keys()))>0:
@@ -16,4 +17,6 @@ def translate(w):
         return "word does not exist.Please double check it"
 
 word = input("Enter a word:")
-print(translate(word.lower()))
+output = translate(word)
+for item in output:
+    print(item)
